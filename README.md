a8a9563194815437d6fdc3987d280b919ee9805a
Dave Lochhead <dave.lochhead@dogdigital.com>
2013-05-28 13:05:58 +0100

    Init Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 swf-template
 ============
+
+SWF Template is a simple HTML5 Boilerplate / Bootstrap template for previewing SWFs.
+
+It's designed so it can be quickly deployed using [Fetch](http://net.tutsplus.com/articles/news/introducing-nettuts-fetch/) via [Sublime Text 2] (http://www.sublimetext.com/2)
---
+++ js/main.js
@@ -0,0 +1,11 @@
+function cacheBuster()
+{
+	return Math.round(Math.random() * 1000000)
+}
+
+
+var flashvars = {};
+var params = {};
+var attributes = {};
+swfobject.embedSWF("swf/SWF1.swf?" + cacheBuster(), "swf1", "400", "400", "8.0.0", false, flashvars, params, attributes);
+// swfobject.embedSWF("swf/SWF2.swf?" + cacheBuster(), "swf2", "400", "400", "8.0.0", false, flashvars, params, attributes);
